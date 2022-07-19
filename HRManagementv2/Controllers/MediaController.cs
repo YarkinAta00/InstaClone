c244aadc0543433646222f1b4fa30ae5a04f5b9e
Yasin Can <yasincan_@hotmail.com.tr>
2022-07-19 16:31:29 +0300

    Profil resimleri güncellendi.

---
+++ HRManagementv2/Controllers/MediaController.cs
@@ -0,0 +1,55 @@
+﻿using HRManagementv2.Data;
+using HRManagementv2.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace HRManagementv2.Controllers
+{
+    public class MediaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public MediaController (ApplicationDbContext db, IWebHostEnvironment hostEnvironment)
+        {
+            _db = db;
+            this._hostEnvironment = hostEnvironment;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MediaId,CandidateId,mediaFile")] Media mediaModel)
+        {
+            if (ModelState.IsValid)
+            {
+                string wwwRootPath = _hostEnvironment.WebRootPath;
+                string fileName = Path.GetFileNameWithoutExtension(mediaModel.mediaFile.FileName);
+                string extension = Path.GetExtension(mediaModel.mediaFile.FileName);
+                mediaModel.Photo =fileName =  fileName + DateTime.Now.ToString("yymmssfff") + extension;
+                string path = Path.Combine(wwwRootPath + "/media/photo", fileName);
+                using (var fileStream = new FileStream(path,FileMode.Create))
+                {
+                    await mediaModel.mediaFile.CopyToAsync(fileStream);
+                }
+
+                _db.Add(mediaModel);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            else
+                return View();
+
+        
+        }
+
+
+    }
+}
---
+++ HRManagementv2/Models/Media.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HRManagementv2.Models
+{
+    public class Media
+    {
+        public int MediaId { get; set; }
+        public int CandidateId { get; set; }
+        [DisplayName("Image Name") ]
+        public string? Photo { get; set; }
+        public string? Resume { get; set; }
+        [NotMapped]
+        [DisplayName("Upload File")]
+        public IFormFile mediaFile { get; set; }
+    }
+}
