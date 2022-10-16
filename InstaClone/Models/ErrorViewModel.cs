8ff177f6cb3937318f4e08ff904699b837b22e8e
yarkinata <yarkinata6268@gmail.com>
2022-10-16 23:00:17 +0300

    creation
    project created

---
+++ InstaClone/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace InstaClone.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
